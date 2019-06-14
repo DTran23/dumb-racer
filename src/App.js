10f64f47de3ab42cd0e2f7a95e8c58b7035823ff
Matt <kimchiadobo@gmail.com>
2019-06-14 16:49:37 -0400

    fixing component to listen for events with a faster function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      words: [],
+      typed: '',
+      index: 0,
+      fullPhrase:
+        'Your self-image is the result of all you have given your subconscious mind as a database, so regardless of your background, what you are willing to become is the only reality that counts.',
+      color: 'white',
+      wordIndex: '0'
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ words: this.state.fullPhrase.split(' ') });
+    const word = this.state.fullPhrase.split(' ')[0] + ' ';
+    this.setState({ word });
+
+    document.addEventListener('input', this._validate, false);
+  }
+
+  componentDidUpdate() {
+    const test = document.getElementById('test-id').value;
+    console.log(test);
+  }
+
+  _validate = e => {
+    e.preventDefault();
+
+    const newEvent = e;
+
+    if (e.target.value !== this.state.word.substring(0, e.target.value.length)) {
+      this.setState({ color: 'red' });
+    } else {
+      this.setState({ color: 'white' });
+    }
+
+    if (e.target.value === this.state.word) {
+      newEvent.target.value = '';
+
+      const index = this.state.index + 1;
+      const word = this.state.words[index] + ' ';
+
+      this.setState({ index, word, color: 'white' });
+    }
+  };
+
+  render() {
+    const style = {
+      color: this.state.color
+    };
+
+    // onKeyUp={this._validate}
+    // onChange={this._checkInput}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.fullPhrase}</p>
+          <p style={style}>{this.state.word}</p>
+          <input id="test-id" />
+        </header>
+      </div>
+    );
+  }
+}
