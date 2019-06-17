89b211959809bb2cd226c5c7de38a2a29388f779
Matt <kimchiadobo@gmail.com>
2019-06-17 17:49:03 -0400

    refactoring code

--- type-server/routes/index.js
+++ type-server/routes/index.js
@@ -4,16 +4,15 @@ const User = require('./../model/User');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  try {
-    const test = new User({ name: 'justn' });
 
-    const { name } = await test.save();
-
+  new User.save().then(({name}) => {
     console.log(name);
+
     res.status(200).json({ success: true, name });
-  } catch (e) {
+  }).catch((e) => {
     res.status(400).json({ message: e.message });
-  }
+  });
+  
 });
 
 module.exports = router;
