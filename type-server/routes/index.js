13b6ce066655edb3be1427729f695573185b7236
Matt <kimchiadobo@gmail.com>
2019-06-17 17:49:54 -0400

    fixing error with previous change:

--- type-server/routes/index.js
+++ type-server/routes/index.js
@@ -5,14 +5,14 @@ const User = require('./../model/User');
 /* GET home page. */
 router.get('/', async (req, res, next) => {
 
-  new User.save().then(({name}) => {
+  new User({ name: 'Justin' }).save().then(({name}) => {
     console.log(name);
 
     res.status(200).json({ success: true, name });
   }).catch((e) => {
     res.status(400).json({ message: e.message });
   });
-  
+
 });
 
 module.exports = router;
